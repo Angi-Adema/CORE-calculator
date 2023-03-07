2d00a5b1f3a4c6e760db6f447873704145b04690
CoderGirl <angi.adema@gmail.com>
2023-03-07 11:53:22 -0700

    updated the script.js file to include code to sum the amounts typed in by users and updated the styles.css and index.html pages to fix responsiveness

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ const directLabor = document.getElementById("directLabor");
 const shippingCost = document.getElementById("shippingCost");
 const salesCommission = document.getElementById("salesCommission");
 const calculateButton = document.getElementById("calculate");
+const finalTotal = document.getElementById("final-total");
 
-function sumCalculator() {
+function sumCalculator(e) {
+  e.preventDefault();
+  const rawMaterialVal = parseFloat(rawMaterials.value);
+  const directLaborVal = parseFloat(directLabor.value);
+  const shippingCostVal = parseFloat(shippingCost.value);
+  const salesCommissionVal = parseFloat(salesCommission.value);
+
   const arr = [
-    "rawMaterials",
-    "directLabor",
-    "shippingCost",
-    "salesCommission",
+    rawMaterialVal,
+    directLaborVal,
+    shippingCostVal,
+    salesCommissionVal,
   ];
 
-  const initialValue = 0;
-  const sumWithInitial = arr.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    initialValue
-  );
-  console.log(sumWithInitial);
+  const totalAmount = arr.reduce((partialSum, a) => partialSum + a, 0);
+  finalTotal.textContent = "$" + totalAmount;
 }
+
+calculateButton.addEventListener("submit", sumCalculator);

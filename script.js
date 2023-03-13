c2d0c6bf8492ba7d41e757effb67e7053e410688
CoderGirl <angi.adema@gmail.com>
2023-03-13 14:52:01 -0600

    removed sales commission field from the index.html and updated the calculation on script.js to remove sales commission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const rawMaterials = document.getElementById("rawMaterials");
 const directLabor = document.getElementById("directLabor");
 const shippingCost = document.getElementById("shippingCost");
-const salesCommission = document.getElementById("salesCommission");
 const jobHours = document.getElementById("jobHours");
 const materialsMarkup = document.getElementById("materialsMarkup");
 const laborMarkup = document.getElementById("laborMarkup");
@@ -13,18 +12,46 @@ function calculator(e) {
   const rawMaterialVal = parseFloat(rawMaterials.value);
   const directLaborVal = parseFloat(directLabor.value);
   const shippingCostVal = parseFloat(shippingCost.value);
-  const salesCommissionVal = parseFloat(salesCommission.value);
-  const jobHoursVal = parseFloat();
+  const jobHoursVal = parseFloat(jobHours.value);
+  const materialsMarkupVal = parseFloat(materialsMarkup.value);
+  const laborMarkupVal = parseFloat(laborMarkup.value);
 
   const arr = [
     rawMaterialVal,
     directLaborVal,
     shippingCostVal,
-    salesCommissionVal,
+    jobHoursVal,
+    materialsMarkupVal,
+    laborMarkupVal,
   ];
 
-  const totalAmount = arr.reduce((partialSum, a) => partialSum + a, 0);
-  finalTotal.textContent = "$" + totalAmount;
+  //   const totalAmount = arr.reduce((partialSum, a) => partialSum + a, 0);
+  //   finalTotal.textContent = "$" + totalAmount;
+  // }
+
+  function calculateGrossHourly(
+    rawMaterialVal,
+    directLaborVal,
+    shippingCostVal,
+    jobHoursVal,
+    materialsMarkupVal,
+    laborMarkupVal
+  ) {
+    const materialsCost = rawMaterialVal / (1 - materialsMarkupVal);
+    const laborAndMarkup =
+      directLaborVal / (1 - laborMarkupVal);
+    const shippingCost = shippingCostVal;
+    const grossHourlyTotal = materialsCost + laborAndMarkup + shippingCost / jobHoursVal;
+    grossHourlyTotal.textContent = "$" + totalAmount;
+  };
+
+  // function calculateMinHourly(rawMaterialVal, directLaborVal, jobHoursVal, shippingCostVal, salesCommissionVal) {
+  //   const materialsCost = rawMaterialVal / (laborMarkupVal * jobHoursVal) + shippingCostVal;
+  //   const total = materialsCost / (1 - salesCommissionVal);
+  //   const minHourly = total /
+  // }
 }
+
+console.log(calculateGrossHourly);
 
 calculateButton.addEventListener("submit", calculator);
